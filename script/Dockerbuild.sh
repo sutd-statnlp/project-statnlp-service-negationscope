af94c3ed53a48ecc82a4cae647b8333dc3a20c69
Ai Nguyen <ainguyenkaka@gmail.com>
2018-07-27 16:58:34 +0800

    uupdate docker scripts

--- script/Dockerbuild.sh
+++ script/Dockerbuild.sh
@@ -1,2 +1,2 @@
 docker build -t statnlp/service-negationscope .
-docker tag  statnlp/service-negationscope statnlp/service-negationscope:0.0.0
+docker tag  statnlp/service-negationscope statnlp/service-negationscope:$APP_VERSION
